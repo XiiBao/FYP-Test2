b71567c23298ea810c2dad4ba77112b1a3555b25
THAM XI WEN <231297k@mymail.nyp.edu.sg>
2026-05-06 17:38:27 +0800

    two tests

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
+import os
 import pysd
-import streamlit as st
 
-print("Setup works!")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(BASE_DIR, 'water.mdl')
+
+print(file_path)
+print(os.path.exists(file_path))
+
+model = pysd.read_vensim(file_path)
+
+results = model.run()
+
+# 3. View the results
+print(results.head())
